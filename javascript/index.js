e33b4f606973ba66ca129decc327c50551541617
valenerramouspe <valen_erramouspe@hotmail.com>
2023-01-12 16:15:09 -0300

    Added functionality to dropdown

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,55 @@
 
 function updateTime(){
-let losAngelesTime = document.querySelector("#los-angeles .time")
-let losAngelesDate = document.querySelector("#los-angeles .date")
-let losAngelesNow = moment().tz("America/Los_Angeles")
-losAngelesDate.innerHTML=losAngelesNow.format("dddd, MMMM D, YYYY")
-losAngelesTime.innerHTML=losAngelesNow.format("h:mm:ss [<small>]A[</small>]")
+    let losAngelesElement = document.querySelector ("#los-angeles")
+    if(losAngelesElement){
+        let losAngelesTime = losAngelesElement.querySelector("#los-angeles .time")
+        let losAngelesDate = losAngelesElement.querySelector("#los-angeles .date")
+        let losAngelesNow = moment().tz("America/Los_Angeles")
+        losAngelesDate.innerHTML=losAngelesNow.format("dddd, MMMM D, YYYY")
+        losAngelesTime.innerHTML=losAngelesNow.format("h:mm:ss [<small>]A[</small>]")
+    }
 
-let buenosAiresTime = document.querySelector("#buenos-aires .time")
-let buenosAiresDate = document.querySelector("#buenos-aires .date")
-let buenosAiresNow = moment().tz("America/Argentina/Buenos_Aires")
-buenosAiresDate.innerHTML=buenosAiresNow.format("dddd, MMMM D, YYYY")
-buenosAiresTime.innerHTML=buenosAiresNow.format("h:mm:ss [<small>]A[</small>]")
+    let buenosAiresElement = document.querySelector ("#buenos-aires")
+    if(buenosAiresElement){
+        let buenosAiresTime = buenosAiresElement.querySelector("#buenos-aires .time")
+        let buenosAiresDate = buenosAiresElement.querySelector("#buenos-aires .date")
+        let buenosAiresNow = moment().tz("America/Argentina/Buenos_Aires")
+        buenosAiresDate.innerHTML=buenosAiresNow.format("dddd, MMMM D, YYYY")
+        buenosAiresTime.innerHTML=buenosAiresNow.format("h:mm:ss [<small>]A[</small>]")
+    }
 
-let copenhagenTime = document.querySelector("#copenhagen .time")
-let copenhagenDate = document.querySelector("#copenhagen .date")
-let copenhagenNow = moment().tz("Europe/Copenhagen")
-copenhagenDate.innerHTML=copenhagenNow.format("dddd, MMMM D, YYYY")
-copenhagenTime.innerHTML=copenhagenNow.format("h:mm:ss [<small>]A[</small>]")
+    let copenhagenElement = document.querySelector ("#copenhagen")
+    if(copenhagenElement){
+        let copenhagenTime = copenhagenElement.querySelector("#copenhagen .time")
+        let copenhagenDate = copenhagenElement.querySelector("#copenhagen .date")
+        let copenhagenNow = moment().tz("Europe/Copenhagen")
+        copenhagenDate.innerHTML=copenhagenNow.format("dddd, MMMM D, YYYY")
+    copenhagenTime.innerHTML=copenhagenNow.format("h:mm:ss [<small>]A[</small>]")
+    }
 }
 
-setInterval(updateTime, 1000)
+
+let cities = document.querySelector(".cities")
+
+let citySelectDropdown = document.querySelector("#city-dropdown")
+
+function changeCity(event){
+    if(event){
+        setInterval( newCityUpdate =>{
+            let timeZone = event.target.value
+            let currentTime =moment().tz(timeZone)
+            cities.innerHTML=`
+                <div class="city"><div>
+                    <h2>
+                        ${timeZone.replace("_", " ").split("/").pop()}
+                    </h2>
+                    <div class="date">${currentTime.format("dddd, MMMM D, YYYY")}</div>
+                </div>
+        <div class="time">${currentTime.format("h:mm:ss [<small>]A[</small>]")}</div>
+    </div>`}, 100)
+    }
+}
+
+citySelectDropdown.addEventListener("change", changeCity)
+
+setInterval(updateTime, 100)
